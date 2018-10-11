1b10e8be5e5e7c112d45fca1d14754dc3daa00e6
rywyr <ginntama1996@hotmail.co.jp>
2018-10-12 00:50:15 +0900

    fasdfasdfasdfasfd

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   get 'home/top'
   get 'home/index'
-  post 'home/index/:uuid' => 'home#index'
+  get 'home/index/:uuid' => 'home#index'
   post 'home/edit'
   post 'home/create'
   post 'home/jcre'
