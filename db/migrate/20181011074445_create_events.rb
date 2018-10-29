2dd0482e7947820893258e69c04018e2d2d7bee4
Tsubasa Imamura <imamura@mk.ces.kyutech.ac.jp>
2018-10-29 11:49:44 +0900

    a

--- db/migrate/20181011074445_create_events.rb
+++ db/migrate/20181011074445_create_events.rb
@@ -3,7 +3,7 @@ class CreateEvents < ActiveRecord::Migration[5.2]
     create_table :events do |t|
       t.string :title, null: false
       t.string :password_digest, limit: 191,  null: false
-      t.string :remember_token,  limit: 191,  
+      t.string :remember_token,  limit: 191
       t.timestamps
     end
   end
