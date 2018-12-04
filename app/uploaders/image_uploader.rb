aeaff3df66e80bc6d17ed9d1a6f60ad4fa1e7dc9
Tsubasa Imamura <imamura@mk.ces.kyutech.ac.jp>
2018-12-04 10:21:22 +0900

    a

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -1,6 +1,6 @@
 # encoding: utf-8
 
-class ImageUploaderUploader < CarrierWave::Uploader::Base
+class ImageUploader < CarrierWave::Uploader::Base
 
   # Include RMagick or MiniMagick support:
   # include CarrierWave::RMagick
--- config/application.rb
+++ config/application.rb
@@ -11,7 +11,7 @@ Bundler.require(*Rails.groups)
 module Mypage
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
-    config.autoload_paths += Dir[Rails.root.join('app', 'uploaders')]
+    #config.autoload_paths += Dir[Rails.root.join('app', 'uploaders')]
     config.load_defaults 5.2
     config.time_zone = 'Tokyo'
     config.active_record.default_timezone = :local
